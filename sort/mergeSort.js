948e1aed3e348653cd0314a45bc4f2c5c60413ab
bahaaEldeen <bahaaeldeen1999@gmail.com>
2019-05-24 01:59:46 +0200

    added merge sort and quick Sort

--- sort/mergeSort.js
+++ sort/mergeSort.js
@@ -1,28 +1,72 @@
 
 
 // split then merge
+function merge(arr,l,m,r){
+    let i, j, k; 
+    let n1 = m - l + 1; 
+    let n2 =  r - m; 
 
-function mergeSort(arr){
-    let x = 2;
-    //console.log(Math.log2(arr.length))
-    for(let j = 0 ; j< Math.ceil(Math.log2(arr.length)); j++){
-        let y = 0;
-        for(let i=0; i<arr.length/2 ; i++){
-            
-            console.log(i*2+y,i*2+1+j+y)
-            if( arr[i+y] > arr[i+j+1] ){
-                let temp = arr[i];
-                arr[i] = arr[i+j+1];
-                arr[i+j+1] = temp;
-            }
-            y++;
-        }
-        x+=2;
-    }
+    /* create temp arrays */
+    let L = new Array(n1);
+    let R  = new Array(n2); 
+  
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++) 
+    L[i] = arr[l + i]; 
+for (j = 0; j < n2; j++) 
+    R[j] = arr[m + 1+ j]; 
     
-return arr;
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray 
+    j = 0; // Initial index of second subarray 
+    k = l; // Initial index of merged subarray 
+    while (i < n1 && j < n2) 
+    { 
+        if (L[i] <= R[j]) 
+        { 
+            arr[k] = L[i]; 
+            i++; 
+        } 
+        else
+        { 
+            arr[k] = R[j]; 
+            j++; 
+        } 
+        k++; 
+    } 
+  
+    /* Copy the remaining elements of L[], if there 
+       are any */
+    while (i < n1) 
+    { 
+        arr[k] = L[i]; 
+        i++; 
+        k++; 
+    } 
+  
+    /* Copy the remaining elements of R[], if there 
+       are any */
+    while (j < n2) 
+    { 
+        arr[k] = R[j]; 
+        j++; 
+        k++; 
+    }
+}
+function mergeSort(arr,left,right){
+    
+    if(left<right){
 
+    let middle =  Math.floor((right+left)/2);
+    mergeSort(arr,left,middle);
+    mergeSort(arr,middle+1,right);
+    
+    merge(arr,left,middle,right);
+
+    }
 
 }
 
-console.log(mergeSort([4,5,812,7,7,7,7,7,36,1309,46,35,8,42,3,1,2]));
+let arr = [4,5,812,7,7,7,7,7,36,1309,46,35,8,42,3,1,2]
+mergeSort(arr , 0 , arr.length-1)
+console.log(arr)
